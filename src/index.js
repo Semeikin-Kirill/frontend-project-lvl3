dc0552c6917f7796236a86c5cb8015aafc55689d
Kirill_Semeykin <semeikinkirill21@gmail.com>
2021-11-29 14:45:33 +0300

    experiment2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/js/dist/modal';
-import runApp from './app';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/dist/modal";
+import runApp from "./app";
 
-module.exports = runApp();
-exports = module.exports;
-exports.program = exports;
+export default runApp();
